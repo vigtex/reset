9cc5f88a2808e5e6e525d42df37633af88a8b804
vigtex <88960502+vigtex@users.noreply.github.com>
2021-08-22 13:49:11 -0400

    Add files via upload
    Módulo 1 - Aula 4

---
+++ apresentacao.js
@@ -0,0 +1,9 @@
+let nomeCompleto = "Virgilio Teixeira";
+let apelido = "Vig";
+let idade = 42;
+let dataNascimento = "18/01/1979";
+let localNascimento = "Porto Alegre";
+let altura = 1.7;
+let trabalhando = true;
+apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido[a] como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + "."
+console.log (apresentacao);
